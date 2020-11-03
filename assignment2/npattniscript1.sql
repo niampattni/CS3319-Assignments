30324710219cea69867ca6826cc2b28c1fc1654e
Niam <npattni@uwo.ca>
2020-11-03 17:55:09 -0500

    Assign2 file1 script created

---
+++ assignment2/npattniscript1.sql
@@ -0,0 +1,15 @@
+SHOW DATABASES;
+DROP DATABASE IF EXISTS npattniassign2db;
+CREATE DATABASE npattniassign2db;
+USE npattniassign2db;
+GRANT USAGE ON *.* TO 'ta'@'localhost';
+DROP USER 'ta'@'localhost';
+CREATE USER 'ta'@'localhost' IDENTIFIED BY 'cs3319';
+GRANT ALL PRIVILEGES ON npattniassign2db.* TO 'ta'@'localhost';
+FLUSH PRIVILEGES;
+SHOW TABLES;
+CREATE TABLE WesternCourse(CourseName VARCHAR(50) NOT NULL, CourseNumber CHAR(6) NOT NULL, Weight CHAR(3) NOT NULL, Suffix CHAR(1), PRIMARY KEY(CourseNumber));
+CREATE TABLE University(OfficialName VARCHAR(50) NOT NULL, City VARCHAR(20) NOT NULL, Province CHAR(2) NOT NULL, Nickname VARCHAR(20) NOT NULL, UniversityID CHAR(2) NOT NULL, PRIMARY KEY(UniversityID));
+CREATE TABLE OtherCourse(CourseName VARCHAR(50) NOT NULL, CourseCode CHAR(10) NOT NULL, Weight CHAR(3) NOT NULL, YearOffered CHAR(1) NOT NULL, UniversityID CHAR(2) NOT NULL, PRIMARY KEY(CourseCode), FOREIGN KEY(UniversityID) REFERENCES University(UniversityID) ON DELETE SET NULL);
+CREATE TABLE EquivalentCourses(WesternCourse CHAR(6) NOT NULL, OutsideCourse CHAR(10) NOT NULL, University VARCHAR(20) NOT NULL, DateDecided VARCHAR(12) NOT NULL, FOREIGN KEY(WesternCourse) REFERENCES WesternCourse(CourseNumber), FOREIGN KEY(OutsideCourse) REFERENCES OutsideCourse(CourseCode), FOREIGN KEY(University) REFERENCES University(UniversityID));
+SHOW TABLES;

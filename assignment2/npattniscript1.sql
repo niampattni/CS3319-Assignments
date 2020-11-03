312ebddba501cf7ba12809881a8e77dbb2455504
Niam <npattni@uwo.ca>
2020-11-03 23:12:25 +0000

    Assign2 first script fixed and ran

--- assignment2/npattniscript1.sql
+++ assignment2/npattniscript1.sql
@@ -8,8 +8,37 @@ CREATE USER 'ta'@'localhost' IDENTIFIED BY 'cs3319';
 GRANT ALL PRIVILEGES ON npattniassign2db.* TO 'ta'@'localhost';
 FLUSH PRIVILEGES;
 SHOW TABLES;
-CREATE TABLE WesternCourse(CourseName VARCHAR(50) NOT NULL, CourseNumber CHAR(6) NOT NULL, Weight CHAR(3) NOT NULL, Suffix CHAR(1), PRIMARY KEY(CourseNumber));
-CREATE TABLE University(OfficialName VARCHAR(50) NOT NULL, City VARCHAR(20) NOT NULL, Province CHAR(2) NOT NULL, Nickname VARCHAR(20) NOT NULL, UniversityID CHAR(2) NOT NULL, PRIMARY KEY(UniversityID));
-CREATE TABLE OtherCourse(CourseName VARCHAR(50) NOT NULL, CourseCode CHAR(10) NOT NULL, Weight CHAR(3) NOT NULL, YearOffered CHAR(1) NOT NULL, UniversityID CHAR(2) NOT NULL, PRIMARY KEY(CourseCode), FOREIGN KEY(UniversityID) REFERENCES University(UniversityID) ON DELETE SET NULL);
-CREATE TABLE EquivalentCourses(WesternCourse CHAR(6) NOT NULL, OutsideCourse CHAR(10) NOT NULL, University VARCHAR(20) NOT NULL, DateDecided VARCHAR(12) NOT NULL, FOREIGN KEY(WesternCourse) REFERENCES WesternCourse(CourseNumber), FOREIGN KEY(OutsideCourse) REFERENCES OutsideCourse(CourseCode), FOREIGN KEY(University) REFERENCES University(UniversityID));
+
+CREATE TABLE WesternCourse(
+	CourseName VARCHAR(50) NOT NULL,
+	CourseNumber CHAR(6) NOT NULL,
+	Weight CHAR(3) NOT NULL,
+	Suffix CHAR(1),
+	PRIMARY KEY(CourseNumber) );
+
+CREATE TABLE University(
+	OfficialName VARCHAR(50) NOT NULL,
+	City VARCHAR(20) NOT NULL,
+	Province CHAR(2) NOT NULL,
+	Nickname VARCHAR(20) NOT NULL,
+	UniversityID CHAR(2) NOT NULL,
+	PRIMARY KEY(UniversityID) );
+
+CREATE TABLE OtherCourse(
+	CourseName VARCHAR(50) NOT NULL,
+	CourseCode CHAR(10) NOT NULL,
+	Weight CHAR(3) NOT NULL,
+	YearOffered CHAR(1) NOT NULL,
+	UniversityID CHAR(2),
+	PRIMARY KEY(CourseCode),
+	FOREIGN KEY(UniversityID) REFERENCES University(UniversityID) ON DELETE SET NULL );
+
+CREATE TABLE EquivalentCourses(
+	WesternCourse CHAR(6),
+	OutsideCourse CHAR(10),
+	University CHAR(2),
+	DateDecided VARCHAR(12) NOT NULL,
+	FOREIGN KEY(WesternCourse) REFERENCES WesternCourse(CourseNumber) ON DELETE SET NULL,
+	FOREIGN KEY(OutsideCourse) REFERENCES OtherCourse(CourseCode) ON DELETE SET NULL,
+	FOREIGN KEY(University) REFERENCES University(UniversityID) ON DELETE SET NULL );
 SHOW TABLES;
